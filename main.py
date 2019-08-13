6697b29e78953da9e0fac0980e3503c2b5710af6
sotis-am <35930547+sotis-am@users.noreply.github.com>
2019-08-13 10:11:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-self.response.write('I update automatically!')
-
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
