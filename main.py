268abf88670744669d0293526b83cfad4f380cce
sotis-am <35930547+sotis-am@users.noreply.github.com>
2019-08-13 10:09:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+self.response.write('I update automatically!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
